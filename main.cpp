6a877406cff3904efa5569dda49a526d6f178e88
Torphedo <ahiatt.cosgrove@gmail.com>
2022-03-12 14:34:42 -0500

    Add extraction function

--- main.cpp
+++ main.cpp
@@ -8,11 +8,28 @@
 #include <tchar.h>
 #include <stdio.h>
 #include <cstdio>
+#include <cstring>
+#include <string>
 #include<chrono>
 #include<thread>
 #pragma comment(lib, "urlmon.lib")
-
 using namespace std;
+
+int extract_zip(string path, string file) {
+	string cmd = "tools\\7z.exe x -o" + path + " " + file + " > NUL";
+
+	char* cmd_char;
+	cmd_char = &cmd[0];
+
+	char* file_char;
+	file_char = &file[0];
+
+	cout << "Extracting...\n";
+	system(cmd_char);
+	remove(file_char);
+	return 0;
+}
+
 int main() {
 
 	system("tools\\TegraRcmSmash.exe tools\\memloader_usb.bin -r --dataini=tools\\ums_sd.ini");
@@ -21,17 +38,14 @@ int main() {
 
     URLDownloadToFile(NULL, L"https://github.com/Atmosphere-NX/Atmosphere/releases/download/1.2.6/atmosphere-1.2.6-master-173d5c2d3+hbl-2.4.1+hbmenu-3.5.0.zip", L"D:\\test\\atmosphere.zip", 0, NULL);
 	cout << "\nDownloaded Atmosphere.\n";
-
-	cout << "Extracting...\n";
-	system("tools\\7z.exe x -oD:\\test D:\\test\\atmosphere.zip > NUL");
-	remove("D:\\test\\atmosphere.zip");
+	extract_zip("D:\\test", "D:\\test\\atmosphere.zip");
 
 	URLDownloadToFile(NULL, L"https://github.com/CTCaer/hekate/releases/download/v5.7.0/hekate_ctcaer_5.7.0_Nyx_1.2.0.zip", L"D:\\test\\hekate.zip", 0, NULL);
 	cout << "Downloaded Hekate.\n";
+	extract_zip("D:\\test", "D:\\test\\hekate.zip");
 
-	cout << "Extracting...\n";
-	system("tools\\7z.exe x -oD:\\test D:\\test\\hekate.zip > NUL");
-	remove("D:\\test\\hekate.zip");
+	URLDownloadToFile(NULL, L"https://github.com/DarkMatterCore/nxdumptool/releases/download/v1.1.15/nxdumptool.nro", L"D:\\test\\switch\\nxdumptool.nro", 0, NULL);
+	URLDownloadToFile(NULL, L"https://github.com/fortheusers/hb-appstore/releases/download/2.2/appstore.nro", L"D:\\test\\switch\\appstore.nro", 0, NULL);
 
 	return 1;
 }
