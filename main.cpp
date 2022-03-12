c3ba926e4b45bb4d2293bbe62560cce38e198858
Torphedo <ahiatt.cosgrove@gmail.com>
2022-03-12 13:02:05 -0500

    Restore commented code

--- main.cpp
+++ main.cpp
@@ -22,16 +22,16 @@ int main() {
     URLDownloadToFile(NULL, L"https://github.com/Atmosphere-NX/Atmosphere/releases/download/1.2.6/atmosphere-1.2.6-master-173d5c2d3+hbl-2.4.1+hbmenu-3.5.0.zip", L"D:\\test\\atmosphere.zip", 0, NULL);
 	cout << "\nDownloaded Atmosphere.\n";
 
-	//system("tools\\7z.exe x -oD:\\test D:\\test\\atmosphere.zip > NUL");
-	//remove("D:\\test\\atmosphere.zip");
-	cout << "Extracted Atmosphere.\n";
+	cout << "Extracting...\n";
+	system("tools\\7z.exe x -oD:\\test D:\\test\\atmosphere.zip > NUL");
+	remove("D:\\test\\atmosphere.zip");
 
 	URLDownloadToFile(NULL, L"https://github.com/CTCaer/hekate/releases/download/v5.7.0/hekate_ctcaer_5.7.0_Nyx_1.2.0.zip", L"D:\\test\\hekate.zip", 0, NULL);
 	cout << "Downloaded Hekate.\n";
 
-	//system("tools\\7z.exe x -oD:\\test D:\\test\\hekate.zip > NUL");
-	//remove("D:\\test\\hekate.zip");
-	cout << "Extracted Hekate.\n";
+	cout << "Extracting...\n";
+	system("tools\\7z.exe x -oD:\\test D:\\test\\hekate.zip > NUL");
+	remove("D:\\test\\hekate.zip");
 
 	return 1;
 }
